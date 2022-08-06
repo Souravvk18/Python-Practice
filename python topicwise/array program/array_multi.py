7d233590299ac41d05492b79f24f5e8cfd90e105
Sourav <souravlakshan6@gmail.com>
2022-08-06 10:57:45 +0530

    folder created

---
+++ python topicwise/array program/array_multi.py
@@ -0,0 +1,23 @@
+arr =[5,2,3,4,6]
+
+mul= 1
+n= 7
+for i in arr:
+    mul= mul*(i % n)
+
+print("the remaining is:", mul %n)
+
+# method -2---
+
+# def remaining(arr, l,n):
+#     mul =1
+#     for i in arr:
+#         mul = mul * (i % n)
+#     print("the remaining is:",mul % n)
+# arr = [2,3,4,5]
+# l= len(arr)
+# n=11
+# remaining(arr,l,n)
+
+# the remaining is: 10
+
